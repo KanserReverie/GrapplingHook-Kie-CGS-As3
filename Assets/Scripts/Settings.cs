c597a5043e513389055781134e41f067dd2abe41
Kieran <kieranc.inbox@gmail.com>
2021-10-24 18:21:46 +1100

    dd

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
-using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
